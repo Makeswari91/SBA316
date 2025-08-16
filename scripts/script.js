475b6bfc8f80d448375e92998828abcaba49bcd3
Makeswari Ramanathan <mahithamakeswari91@gmail.com>
2025-08-16 17:06:01 -0400

    js

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+// Caching elements using getElementById
+const form = document.getElementById('studyForm');
+const resultContainer = document.getElementById('planResult');
+const section = document.getElementById('planSection');
