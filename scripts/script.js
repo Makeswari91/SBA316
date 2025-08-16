d36b2da311304645a39b0c60dde39dc12f3f9c05
Makeswari Ramanathan <mahithamakeswari91@gmail.com>
2025-08-16 17:35:16 -0400

    BOM

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,9 @@ const resultContainer = document.getElementById('planResult');
 const section = document.getElementById('planSection');
 
 // Caching using querySelectorAll
-const inputs = document.querySelectorAll('#studyForm input, #studyForm select');
+const inputs = document.querySelectorAll('#studyForm input, #studyForm select'); 
+
+// BOM property to alert time and engage the user
+setTimeout(() => {
+  alert('Ready to focus and crush your goals?');
+}, 3000);//executes after 3 seconds
