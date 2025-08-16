fcaf499da9eb6a9411c4ef8c94f333b6c0fc7683
Makeswari Ramanathan <mahithamakeswari91@gmail.com>
2025-08-16 17:08:27 -0400

    qsall

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,6 @@
 const form = document.getElementById('studyForm');
 const resultContainer = document.getElementById('planResult');
 const section = document.getElementById('planSection');
+
+// Caching using querySelectorAll
+const inputs = document.querySelectorAll('#studyForm input, #studyForm select');
